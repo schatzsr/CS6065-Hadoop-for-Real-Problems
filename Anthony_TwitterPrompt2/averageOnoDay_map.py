59027079b7a36f92c963426f009c0576475d31db
Anthony Kleiser <kleiseaj@mail.uc.edu>
2015-04-22 13:28:22 +0000

    Working solution for single TXT file. TODO::Work on full Twitter dataset

--- Anthony_TwitterPrompt2/averageOnoDay_map.py
+++ Anthony_TwitterPrompt2/averageOnoDay_map.py
@@ -3,12 +3,16 @@
 import sys
 import string
 import json
+import traceback
 
 for line in sys.stdin:
     tweet_data = json.loads(line)
-    user_info = tweet_data["user"]
-    user_id = user_info["id"]
-    if user_id is 211178363:
-        date_field = tweet_data["created_at"]
-        day = date_field[:3]
-        print 'Ono\t%s' % day
+    try:
+        user_info = tweet_data["user"]
+        user_id = user_info["id"]
+        if user_id == 211178363:
+            date_field = tweet_data["created_at"]
+            day = date_field[:3]
+            print '%s\t1' % day
+    except:
+        #print '%s\t1' % traceback.print_exc()
--- Anthony_TwitterPrompt2/averageOnoDay_reduce.py
+++ Anthony_TwitterPrompt2/averageOnoDay_reduce.py
@@ -15,38 +15,51 @@ sun_count = 0
 for line in sys.stdin:
     (key,val) = line.strip().split('\t',1)
     try:
-        day = val
-        if key == 'Ono':
+        key = str(key)
+        if key == 'Mon':
             total_count += 1
-            if val is "Mon":
-                mon_count += 1
-            if val is "Tue":
-                tue_count += 1
-            if val is "Wed":
-                wed_count += 1
-            if val is "Thu":
-                thu_count += 1
-            if val is "Fri":
-                fri_count += 1
-            if val is "Sat":
-                sat_count += 1
-            if val is "Sun":
-                sun_count += 1
+            mon_count += 1
+        elif key == 'Tue':
+            total_count += 1
+            tue_count += 1
+        elif key == 'Wed':
+            total_count += 1
+            wed_count += 1
+        elif key == 'Thu':
+            total_count += 1
+            thu_count += 1
+        elif key == 'Fri':
+            total_count += 1
+            fri_count += 1
+        elif key == 'Sat':
+            total_count += 1
+            sat_count += 1
+        elif key == 'Sun':
+            total_count += 1
+            sun_count += 1
+        else:
+            print key
     except:
         continue
-       
-average_mon_value = round(float(mon_count)/float(total_count), 3)
-average_tue_value = round(float(tue_count)/float(total_count), 3)
-average_wed_value = round(float(wed_count)/float(total_count), 3)
-average_thu_value = round(float(thu_count)/float(total_count), 3)
-average_fri_value = round(float(fri_count)/float(total_count), 3)
-average_sat_value = round(float(sat_count)/float(total_count), 3)
-average_sun_value = round(float(sun_count)/float(total_count), 3)
 
-print '%s\t%s' % ('Monday Average:',average_mon_value)
-print '%s\t%s' % ('Tuesday Average:',average_tue_value)
-print '%s\t%s' % ('Wednesday Average:',average_wed_value)
-print '%s\t%s' % ('Thursday Average:',average_thu_value)
-print '%s\t%s' % ('Friday Average:',average_fri_value)
-print '%s\t%s' % ('Saturday Average:',average_sat_value)
-print '%s\t%s' % ('Sunday Average:',average_sun_value)
+if mon_count > 0:
+    average_mon_value = round(float(mon_count)/float(total_count), 3)
+    print '%s\t%s' % ('Monday Average:',average_mon_value)
+if tue_count > 0:
+    average_tue_value = round(float(tue_count)/float(total_count), 3)
+    print '%s\t%s' % ('Tuesday Average:',average_tue_value)
+if wed_count > 0:
+    average_wed_value = round(float(wed_count)/float(total_count), 3)
+    print '%s\t%s' % ('Wednesday Average:',average_wed_value)
+if thu_count > 0:
+    average_thu_value = round(float(thu_count)/float(total_count), 3)
+    print '%s\t%s' % ('Thursday Average:',average_thu_value)
+if fri_count > 0:
+    average_fri_value = round(float(fri_count)/float(total_count), 3)
+    print '%s\t%s' % ('Friday Average:',average_fri_value)
+if sat_count > 0:
+    average_sat_value = round(float(sat_count)/float(total_count), 3)    
+    print '%s\t%s' % ('Saturday Average:',average_sat_value)
+if sun_count > 0:
+    average_sun_value = round(float(sun_count)/float(total_count), 3)
+    print '%s\t%s' % ('Sunday Average:',average_sun_value)
