274f8940d4ac19cb3fa871d16b4a27875614058d
Anthony Kleiser <kleiseaj@mail.uc.edu>
2015-04-30 13:15:18 +0000

    Top Tweeter Stats complete, need to reduce the amount of data being passed to reduce.

--- Anthony_TwitterPrompt5/topTweeterStats_map.py
+++ Anthony_TwitterPrompt5/topTweeterStats_map.py
@@ -8,5 +8,24 @@ import traceback
 # QUESTION:
 # What twitter user tweeted the most?  What is the top 5 longest tweeters?  Bottom 5?
 
+user_dict = {}
+
+#text = open('exampletwitterdata.txt', 'r')
+#print 'opened'
 for line in sys.stdin:
-    # TODO:: The problem
+#for line in text:
+    #print 'load'
+    tweet_data = json.loads(line)
+    try:
+        #print 'got json'
+        user_info = tweet_data["user"]
+        #print 'found user'
+        user_name = user_info["screen_name"]
+        #print 'screen_name'
+        tweet_content = tweet_data["text"]
+        #print 'content'
+        tweet_len = len(tweet_content)
+        #print 'length'
+        print str(user_name) + '\t' + '1' + '\t' + str(tweet_len)
+    except:
+        continue
